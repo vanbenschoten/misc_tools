8171aeb66fbbe42d92ef7b40b2fef55a27a2b35b
Andrew Van Benschoten <avbs89@gmail.com>
2014-08-20 07:45:46 -0700

    First completed version

--- diffuse_bins.py
+++ diffuse_bins.py
@@ -2,6 +2,7 @@ import math
 
 def run(args):
 
+  make_miller(args[1])
 
 def make_miller(file):
 
@@ -28,15 +29,39 @@ def make_miller(file):
 
   fin.close()
 
-  #NOTE: get symmetry object!!!
-  cs = self.symmetry
-  ma = miller.array(miller_set=miller.set(cs, indices), data=i_obs, sigmas=sig_i)
+  #Input symmetry object
 
-  return ma
+  #NOTE: get symmetry object!!!
+  cs = cs = crystal.symmetry(unit_cell=(x,y,z,90,90,90), space_group='P 21 21 21')
+  ma = miller.array(miller_set=miller.set(cs, indices), data=i_obs, sigmas=sig_i)
+  ma.set_observation_type_xray_intensity()
+  ma_anom = ma.customized_copy(anomalous_flag=false)
+
+  return ma_anom
 
 
 def get_bin_data(array):
-	
+
+  d_star_power = 1.618034
+
+  n_bins=50
+  binner= array.setup_binner(n_bins=n_bins)
+  I = array.intensities()
+  selections = [binner.selection(i) for i in binner.range_used()]
+  means = [I.select(sel).mean() for sel in selections]
+  log_means = math.log(m) for m in means]
+  centers = binner.bin_centers(d_star_power)
+  d_centers = centers**(-1/d_star_power)
+
+  plot = pygmyplot.xy_plot(centers, log_means, master=tk)
+  plot.axes.set_xticks(centers[::3])
+  plot.axes.set_xticklabels(x_labels[::3])
+  plot.axes.set_xlabel('Resolution ($\\AA$)')
+  plot.axes.set_ylabel('log intensity')
+  plot.canvas.draw()
+
+  TK.Button(tk, text="Quit", command=tk.destroy).pack()
+  tk.mainloop()
 
 
 if __name__ == '__main__':
